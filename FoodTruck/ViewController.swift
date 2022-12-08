28370ae803e2ae3fbe7a723a2327c6b0b9113ad6
wilkins <involucradostudios@gmail.com>
2022-12-08 01:36:37 -0500

    Initial Commit

---
+++ FoodTruck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTruck
+//
+//  Created by wilkins vargas on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
